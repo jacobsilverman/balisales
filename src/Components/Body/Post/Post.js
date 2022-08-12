6f75f446cd674caccf2efbd1af5b2583310af9a8
jacobsilverman <jacob.h.silverman@gmail.com>
2022-08-11 19:08:32 -0700

    minior updates

--- src/Components/Body/Post/Post.js
+++ src/Components/Body/Post/Post.js
@@ -1,36 +1,34 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
 import './Post.css';
 
 import { Link } from 'react-router-dom';
 
-import { Col } from 'react-bootstrap';
+import { Col, Row } from 'react-bootstrap';
 
-function Post({item, title, price, username, condition}) {
+function Post({item, title, price}) {
     // const data = Object.entries(item);
     // const queryParam = data?.reduce((acc, [prop, value]) => {
     //     return acc + `${prop}=${value}${(prop!=='key')?'&':''}`;
     // }, '');
     const queryParam = 'item='+JSON.stringify(item);
 
-    const background = `post-container ${item.brand}`;
+    const backgroundCls = `post-container ${item.brand}`;
+    const priceCls = `price color-${useMemo(() => {return (item.type==='wts')?'red':(item.type==='wtb')?'green':''}, [item.type])}`;
 
     return (
-        <Col xs={3} className={background}>
-            <div>
-                title: {title}
-            </div>
-            <div>
-                price: {price}
-            </div>
-            <div>
-                username: {username}
-            </div>
-            <div>
-                condition: {condition}
-            </div>
-            <Link to={{pathname: '/singlePost', search: queryParam}}>
-                view
+        <Col xs={12} sm={12} md={3} className={backgroundCls} onClick={()=>{return <Link to={{pathname: '/singlePost', search: queryParam}}></Link>}}>
+            <Link className="link" to={{pathname: '/singlePost', search: queryParam}}>
+                <Row> 
+                    <Col xs={11} className="title-text">
+                        {title.toUpperCase()}
+                    </Col>
+                </Row>
+                <Row> 
+                    <Col className={priceCls}>
+                        ${price}
+                    </Col>
+                </Row>
             </Link>
         </Col> 
     );
