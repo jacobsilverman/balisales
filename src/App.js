55eb9f3b774574240605e022ea669af986f006ff
jacobsilverman <jacob.h.silverman@gmail.com>
2022-08-14 14:21:58 -0700

    setting up the ability to create and recieve the data base posts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Routing from './Routing';
+
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from './firebase-config';
 import BuildMocks from './Data/Mocks/BuildMocks.js'
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const postsCollectionRef = collection(db, "posts");
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(postsCollectionRef);
+      setPosts(data.docs.map((doc) => ({ ...doc.data(), id: doc.id})))
+      console.log(data.docs.map((doc) => ({ ...doc.data(), id: doc.id})));
+    }
+    getPosts();
+  }, []);
+
   const MockData = BuildMocks();
 
   return (
--- src/Components/CreatePost/CreatePost.js
+++ src/Components/CreatePost/CreatePost.js
@@ -1,10 +1,44 @@
-import { Fragment } from 'react';
+import { useState } from 'react';
+import { Button, Col, Container, Row } from 'react-bootstrap';
+import { addDoc, collection } from 'firebase/firestore';
+import { auth, db } from '../../firebase-config';
 
 function CreatePost() {
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+
+    const postsCollectionRef = collection(db, "posts");
+    const createPost = async () => {
+        await addDoc(postsCollectionRef, {
+            title,
+            description,
+            author: {name: auth.currentUser.displayName, id: auth.currentUser.uid }
+        });
+    }
+
     return (
-        <Fragment>
-            <div>Create Post</div>
-        </Fragment>
+        <Container>
+            <Row>
+                <Col>
+                    <h3>Create Post</h3>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <label>Title: </label>
+                    <input onChange={(event) => setTitle(event.target.value)} />
+                </Col>
+                <Col>
+                    <label>Description: </label>
+                    <input onChange={(event) => setDescription(event.target.value)} />
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Button onClick={createPost}>Submit</Button>
+                </Col>
+            </Row>
+        </Container>
         );
 }
 
