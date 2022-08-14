2d01a42011fdc524c9b5fb249468313ceff48be9
jacobsilverman <jacob.h.silverman@gmail.com>
2022-08-13 22:24:48 -0700

    making fake background for hover effect

--- src/Components/Body/Post/Post.js
+++ src/Components/Body/Post/Post.js
@@ -20,29 +20,31 @@ function Post({item, viewCount}) {
     const showViewCount = isNaN(12/viewCount);
 
     return (
-        <Col xs={showViewCount ? 12 : 12/viewCount} sm={showViewCount ? 12 : 12/viewCount} md={showViewCount ? 4 : 12/viewCount} className={backgroundCls} onClick={()=>{return <Link to={{pathname: '/singlePost', search: queryParam}}></Link>}}>
-            <Link className="link" to={{pathname: '/singlePost', search: queryParam}}>
-                <Row> 
-                    <Col xs={11} className="title-text">
-                        {item.title.toUpperCase()}
-                    </Col>
-                </Row>
-                <Row className="hidden-text show-onhover left-align">
-                    <Col >
-                        seller: {item.username}
-                    </Col>
-                </Row>
-                <Row className="hidden-text show-onhover left-align">
-                    <Col >
-                        codition: {item.condition}
-                    </Col>
-                </Row>
-                <Row> 
-                    <Col className={priceCls}>
-                        ${item.price}
-                    </Col>
-                </Row>
-            </Link>
+        <Col  className={backgroundCls} xs={showViewCount ? 12 : 12/viewCount} sm={showViewCount ? 12 : 12/viewCount} md={showViewCount ? 4 : 12/viewCount} onClick={()=>{return <Link to={{pathname: '/singlePost', search: queryParam}}></Link>}}>
+            <div className='fake-background'>
+                <Link className="link" to={{pathname: '/singlePost', search: queryParam}}>
+                    <Row> 
+                        <Col xs={11} className="title-text">
+                            {item.title.toUpperCase()}
+                        </Col>
+                    </Row>
+                    <Row className="hidden-text show-onhover left-align">
+                        <Col >
+                            seller: {item.username}
+                        </Col>
+                    </Row>
+                    <Row className="hidden-text show-onhover left-align">
+                        <Col >
+                            codition: {item.condition}
+                        </Col>
+                    </Row>
+                    <Row> 
+                        <Col className={priceCls}>
+                            ${item.price}
+                        </Col>
+                    </Row>
+                </Link>
+            </div>
         </Col> 
     );
 }
