4701c1075c5f023a60aa8a7e3ea4e83496ae29aa
jacobsilverman <jacob.h.silverman@gmail.com>
2022-08-29 17:40:01 -0700

    making popover account preferences in the header nav bar

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 import './Header.scss';
 
-import { Col, Container, Row } from 'react-bootstrap';
+import { Col, Container, OverlayTrigger, Popover, Row } from 'react-bootstrap';
 
 import { auth, provider } from '../../firebase-config';
 import { signInWithPopup, signOut } from 'firebase/auth';
@@ -17,6 +17,7 @@ function Header({isAuth, setIsAuth}) {
     const signUserOut = () => {
         signOut(auth, provider).then(() => {
             setIsAuth(false);
+            setShowAccount(false);
             navigate("/");
             localStorage.clear();
         })
@@ -32,6 +33,52 @@ function Header({isAuth, setIsAuth}) {
     const [showAccount, setShowAccount] = useState(false);
 
     const accountVisibilityCls = `account-dropdown ${showAccount ? "visible" : 'hidden'}`;
+
+    const popover = (
+        <Popover className="popover-container" id="popover-basic" title="Account Info">
+            <Row className={accountVisibilityCls}>
+                <Col  xs={12}>
+                    <Row>
+                        <Col xs={4} className="popover-item">
+                            <div className="center">Posts</div>
+                            <Button>
+                                <Link className="white" to={{pathname: '/account'}}>
+                                    <svg className="" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="ReceiptLongIcon" tabIndex="-1" title="ReceiptLong"><path d="M19.5 3.5 18 2l-1.5 1.5L15 2l-1.5 1.5L12 2l-1.5 1.5L9 2 7.5 3.5 6 2v14H3v3c0 1.66 1.34 3 3 3h12c1.66 0 3-1.34 3-3V2l-1.5 1.5zM19 19c0 .55-.45 1-1 1s-1-.45-1-1v-3H8V5h11v14z"></path><path d="M9 7h6v2H9zm7 0h2v2h-2zm-7 3h6v2H9zm7 0h2v2h-2z"></path></svg>
+                                </Link>
+                            </Button>
+                            
+                        </Col>
+                        <Col xs={4} className="popover-item">
+                            <div className="center">Profile</div>
+                            <Button>
+                                <svg className="" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="AttributionIcon" tabIndex="-1" title="Attribution"><path d="M12 8.5c-.91 0-2.75.46-2.75 1.38v4.62h1.5V19h2.5v-4.5h1.5V9.88c0-.91-1.84-1.38-2.75-1.38z"></path><path d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"></path><circle cx="12" cy="6.5" r="1.5"></circle></svg>
+                            </Button>
+                            
+                        </Col>
+                        <Col xs={4} className="popover-item">
+                            
+                            <div className="center">Settings</div>
+                            <Button>
+                                <svg className="" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="SettingsIcon" tabIndex="-1" title="Settings"><path d="M19.14 12.94c.04-.3.06-.61.06-.94 0-.32-.02-.64-.07-.94l2.03-1.58c.18-.14.23-.41.12-.61l-1.92-3.32c-.12-.22-.37-.29-.59-.22l-2.39.96c-.5-.38-1.03-.7-1.62-.94l-.36-2.54c-.04-.24-.24-.41-.48-.41h-3.84c-.24 0-.43.17-.47.41l-.36 2.54c-.59.24-1.13.57-1.62.94l-2.39-.96c-.22-.08-.47 0-.59.22L2.74 8.87c-.12.21-.08.47.12.61l2.03 1.58c-.05.3-.09.63-.09.94s.02.64.07.94l-2.03 1.58c-.18.14-.23.41-.12.61l1.92 3.32c.12.22.37.29.59.22l2.39-.96c.5.38 1.03.7 1.62.94l.36 2.54c.05.24.24.41.48.41h3.84c.24 0 .44-.17.47-.41l.36-2.54c.59-.24 1.13-.56 1.62-.94l2.39.96c.22.08.47 0 .59-.22l1.92-3.32c.12-.22.07-.47-.12-.61l-2.01-1.58zM12 15.6c-1.98 0-3.6-1.62-3.6-3.6s1.62-3.6 3.6-3.6 3.6 1.62 3.6 3.6-1.62 3.6-3.6 3.6z"></path></svg>
+                               
+                            </Button>
+                        </Col>
+                    </Row>
+                    
+                    <Row>
+                        <Button onClick={signUserOut}>
+                            <Col className="right" xs={10}>
+                                <span>Logout</span>
+                            </Col>
+                            <Col className="right" xs={2}>
+                                <svg className="" focusable="false" aria-hidden="true" viewBox="0 0 40 24" data-testid="LogoutIcon" tabIndex="-1" title="Logout"><path d="m17 7-1.41 1.41L18.17 11H8v2h10.17l-2.58 2.58L17 17l5-5zM4 5h8V3H4c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h8v-2H4V5z"></path></svg>
+                            </Col>
+                        </Button>
+                    </Row>
+                </Col>
+            </Row>
+        </Popover>
+      );
 
     return (
         <Fragment>
@@ -51,43 +98,15 @@ function Header({isAuth, setIsAuth}) {
                         </Col>
                         <Col xs={4} sm={4} md={4} className="login-padding">
                             <Row>
-                                <Col className="right pointer">
+                                <Col xs={12} className="right pointer">
                                     {!isAuth
                                     ? <Button onClick={signInWithGoogle}>Login</Button>
-                                    : <Button onClick={() => setShowAccount(!showAccount)}>Account</Button>}
-                                </Col>
-                            </Row>
-                            <Row className={accountVisibilityCls}>
-                                <Col  xs={12}>
-                                    <Row>
-                                        <Col xs={2}>
-                                            <Button>
-                                                <Link className="white" to={{pathname: '/account'}}>
-                                                    <svg className="MuiSvgIcon-root MuiSvgIcon-fontSizeSmall MuiSvgIcon-root css-zjt8k" focusable="false" aria-hidden="true" viewBox="0 0 44 24" data-testid="ReceiptLongIcon" tabIndex="-1" title="ReceiptLong"><path d="M19.5 3.5 18 2l-1.5 1.5L15 2l-1.5 1.5L12 2l-1.5 1.5L9 2 7.5 3.5 6 2v14H3v3c0 1.66 1.34 3 3 3h12c1.66 0 3-1.34 3-3V2l-1.5 1.5zM19 19c0 .55-.45 1-1 1s-1-.45-1-1v-3H8V5h11v14z"></path><path d="M9 7h6v2H9zm7 0h2v2h-2zm-7 3h6v2H9zm7 0h2v2h-2z"></path></svg>
-                                                </Link>
-                                            </Button>
-                                        </Col>
-                                        <Col xs={2}>
-                                            <Button>
-                                                <svg className="MuiSvgIcon-root MuiSvgIcon-fontSizeSmall MuiSvgIcon-root css-zjt8k" focusable="false" aria-hidden="true" viewBox="0 0 44 24" data-testid="AttributionIcon" tabIndex="-1" title="Attribution"><path d="M12 8.5c-.91 0-2.75.46-2.75 1.38v4.62h1.5V19h2.5v-4.5h1.5V9.88c0-.91-1.84-1.38-2.75-1.38z"></path><path d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"></path><circle cx="12" cy="6.5" r="1.5"></circle></svg>
-                                            </Button>
-                                        </Col>
-                                        <Col xs={2}>
-                                            <Button>
-                                                <svg className="MuiSvgIcon-root MuiSvgIcon-fontSizeSmall MuiSvgIcon-root css-zjt8k" focusable="false" aria-hidden="true" viewBox="0 0 44 24" data-testid="SettingsIcon" tabIndex="-1" title="Settings"><path d="M19.14 12.94c.04-.3.06-.61.06-.94 0-.32-.02-.64-.07-.94l2.03-1.58c.18-.14.23-.41.12-.61l-1.92-3.32c-.12-.22-.37-.29-.59-.22l-2.39.96c-.5-.38-1.03-.7-1.62-.94l-.36-2.54c-.04-.24-.24-.41-.48-.41h-3.84c-.24 0-.43.17-.47.41l-.36 2.54c-.59.24-1.13.57-1.62.94l-2.39-.96c-.22-.08-.47 0-.59.22L2.74 8.87c-.12.21-.08.47.12.61l2.03 1.58c-.05.3-.09.63-.09.94s.02.64.07.94l-2.03 1.58c-.18.14-.23.41-.12.61l1.92 3.32c.12.22.37.29.59.22l2.39-.96c.5.38 1.03.7 1.62.94l.36 2.54c.05.24.24.41.48.41h3.84c.24 0 .44-.17.47-.41l.36-2.54c.59-.24 1.13-.56 1.62-.94l2.39.96c.22.08.47 0 .59-.22l1.92-3.32c.12-.22.07-.47-.12-.61l-2.01-1.58zM12 15.6c-1.98 0-3.6-1.62-3.6-3.6s1.62-3.6 3.6-3.6 3.6 1.62 3.6 3.6-1.62 3.6-3.6 3.6z"></path></svg>
-                                            </Button>
-                                        </Col>
-                                        <Col xs={2}>
-                                            <Button>
-                                                <svg className="MuiSvgIcon-root MuiSvgIcon-fontSizeSmall css-vubbuv" focusable="false" aria-hidden="true" viewBox="0 0 44 24" data-testid="LockOutlinedIcon"><path d="M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zM9 6c0-1.66 1.34-3 3-3s3 1.34 3 3v2H9V6zm9 14H6V10h12v10zm-6-3c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2z"></path></svg>
-                                            </Button>
-                                        </Col>
-                                        <Col xs={2}>
-                                            <Button onClick={signUserOut}>
-                                                <svg className="MuiSvgIcon-root MuiSvgIcon-fontSizeSmall MuiSvgIcon-root MuiSvgIcon-fontSizeLarge css-zjt8k" focusable="false" aria-hidden="true" viewBox="0 0 44 24" data-testid="LogoutIcon" tabIndex="-1" title="Logout"><path d="m17 7-1.41 1.41L18.17 11H8v2h10.17l-2.58 2.58L17 17l5-5zM4 5h8V3H4c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h8v-2H4V5z"></path></svg>
-                                            </Button>
-                                        </Col>
-                                    </Row>
+                                    : 
+                                    <OverlayTrigger trigger="click" placement="bottom" overlay={popover}>
+                                        <Button onClick={() => setShowAccount(!showAccount)}>
+                                            <div className="right account-profile" style={{backgroundImage: `url("https://firebasestorage.googleapis.com/v0/b/balisongsales.appspot.com/o/files%2Fz01Eer1ynS5ePoKpWPqj?alt=media&token=78503e7e-6700-440f-b0b6-6f579cba0552")`}} />
+                                        </Button>
+                                    </OverlayTrigger>}
                                 </Col>
                             </Row>
                         </Col>
