0fa4a668b2c81ce8a889d227bd77d0cfa524ceec
jacobsilverman <jacob.h.silverman@gmail.com>
2022-09-01 23:45:00 -0700

    improving the background

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -109,7 +109,7 @@ function Header({isAuth, setIsAuth}) {
                         <Col xs={4} sm={4} md={4} className="center title">
                             <h1 style={{fontSize:"40px",fontFamily:"roboto",paddingTop:"10px"}}>Sales</h1>
                         </Col>
-                        <Col xs={4} sm={4} md={4} className="login-container">
+                        <Col xs={4} sm={4} md={4}>
                             <Row>
                                 <Col xs={12} className="right">
                                     {!isAuth
