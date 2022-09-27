eba3b47434f6be20aebc75f49b7785d78e658d5e
jacobsilverman <jacob.h.silverman@gmail.com>
2022-09-27 16:04:26 -0700

    minor styling to discussion page

--- src/Components/Discuss/Discuss.js
+++ src/Components/Discuss/Discuss.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Card, Col, Container, Row } from 'react-bootstrap';
+import { Button, Card, Col, Container, Row } from 'react-bootstrap';
 
 import { addComment, createDiscussion, deleteComment, getDiscussions } from '../../Data/Services/Discussion.js';
 
@@ -28,11 +28,6 @@ var Discuss = () => {
                         <div className="card-body">
                             <h4>As an ongoing project there are many different routes to facilitate the needs of the users on this site. Here the community can help determine which pathes to prioritize and which improvements are obselete.</h4>
                         </div>
-                        <div className="card-footer">
-                            <h5>
-                                Please feel free to add a discussion, or comment on previous ones
-                            </h5>
-                        </div>
                     </Card>
                     <Card>
                         <div className="card-header center">
@@ -42,7 +37,7 @@ var Discuss = () => {
                             <textarea rows="4" style={{width: "100%", height: "100%"}} onChange={(e) => setNewDiscussion(e.target.value)} />
                         </div>
                         <div className="card-footer center">
-                            <button className="btn btn-primary" onClick={() => createDiscussion(newDiscussion)}>submit</button>
+                            {(uid) ? <button className="btn btn-primary" onClick={() => createDiscussion(newDiscussion)}>submit</button> : "please login to create discussion"}
                         </div>
                     </Card>
                 </div>
@@ -50,35 +45,46 @@ var Discuss = () => {
             <Row>
                 <div style={{marginTop:"20px"}}>
                     {discussions.map((item, index) => {
+                        console.log("item: ", item);
                         return (
                             <div className="card" key={index} style={{margin:"20px 0 20px 0"}}>
                                 <div  className="card-header">
-                                    {item?.displayName}
+                                    <Row>
+                                        <Col xs={11}>{item?.displayName}</Col>
+                                        {item.userId === uid  && <Col xs={1} className="right"><button className="btn btn-danger" style={{fontSize: '9px'}}>x</button></Col>}
+                                    </Row>
                                 </div>
-                                <div className="card-text center">
+                                <h4 className="card-text center">
                                     {item?.body}
-                                </div>
+                                </h4>
                                 <div className="card-footer">
                                     {item?.comments?.map((ele, index) => {
                                         return (
                                             <Row className="comment-container">
-                                                <Col xs={11} className="dialog-text">
-                                                    <Row style={{fontSize: "12px"}} xs={2}>{ele.displayName}</Row>
+                                                <Col xs={12} className="dialog-text">
+                                                    <Row style={{fontSize: "12px"}} xs={2}>
+                                                        <Col xs={11}>
+                                                            {ele.displayName}
+                                                        </Col>
+                                                        <Col xs={1} className="right" style={{paddingRight: "0px"}}>
+                                                            {ele.uid === uid  && <button className="btn btn-danger" style={{fontSize: "9px", borderTopRightRadius: "0px"}} onClick={() => deleteComment(item, index)}>x</button>}
+                                                        </Col>
+                                                    </Row>
                                                     <Row style={{margin: "5px 0px 5px 0px"}} xs={8}>{ele.comment}</Row>
                                                 </Col>
                                                 {/* {ele.uid === uid  && <Col xs={1}><button className="btn btn-primary" style={{fontSize: "10px"}} onClick={() => deleteComment(item, index)}>edit</button></Col>} */}
-                                                {ele.uid === uid  && <Col xs={1}><button className="btn btn-danger" style={{fontSize: "10px"}} onClick={() => deleteComment(item, index)}>delete</button></Col>}
+                                                
                                             </Row>
                                         )
                                     })}
-                                    <Row className="add-comment-container">  
+                                    {uid && <Row className="add-comment-container">  
                                         <Col xs={11}>
                                             <input style={{width:"100%"}} onChange={(e) => setCurrentComment(e.target.value)} />
                                         </Col>
                                         <Col xs={1}>
-                                            <button className="btn btn-primary" style={{fontSize: "10px"}} onClick={() => {console.log(item, currentComment);addComment(item, currentComment)}}>Comment</button>
+                                            <button className="btn btn-primary" style={{fontSize: "10px"}} onClick={() => {addComment(item, currentComment)}}>Comment</button>
                                         </Col>
-                                    </Row>
+                                    </Row>}
                                 </div>
                             </div>
                         )
