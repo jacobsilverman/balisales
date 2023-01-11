8fdb64db56dc762ab358426126d60503d985e6b3
Jacob Silverman <57313642+jacobsilverman@users.noreply.github.com>
2023-01-11 13:15:20 -0800

    styling the selected post page

--- src/Components/SelectPost/SelectPost.js
+++ src/Components/SelectPost/SelectPost.js
@@ -1,6 +1,9 @@
 import { Fragment, useEffect, useState } from 'react';
 
+
+import { getProfilePicture } from '../../Data/Services/userInfo.js';
 import { getPost } from '../../Data/Services/PostInfo.js';
+import defaultProfile from '../../Data/Images/default-profile.jpg';
 
 import './SelectPost.scss';
 
@@ -18,83 +21,71 @@ function SelectPost() {
     const [displayImage, setDisplayImage] = useState(0);
     const [displayPost, setDisplayPost] = useState({});
 
+    const [profilePic, setProfilePic] = useState();
+
     useEffect(() => {
         getPost(params.id).then((data) => {
+            var dateCreated = new Date(data?.timeStamp).toLocaleDateString();
+
             setDisplayPost(data);
-            setCreatedData(new Date(data?.timeStamp).toUTCString());
+            setCreatedData(dateCreated);
         });
     }, []);
 
+    useEffect(() => {
+        getProfilePicture(displayPost?.author?.id).then((result) => {
+            setProfilePic(result);
+        }).catch(() => {
+            setProfilePic(defaultProfile);
+        });
+    }, [displayPost]);
+
+
     return (
         <Fragment>
-            {displayPost?.urls && <Col xs={11} sm={11} md={5} className="image-container" style={{backgroundImage: `url(${displayPost?.urls[displayImage]})`}}>
+            <Col xs={12} className="display-title center" >
+                <h1>{displayPost?.title}</h1>
+            </Col>
+
+            {displayPost?.urls && <Col xs={11} sm={11} md={5} className="image-container align-items-end" style={{backgroundImage: `url(${displayPost?.urls[displayImage]})`}}>
+                <Link className="link-wrapper" to={{pathname: '/profile', search: "id=" + displayPost?.author?.id}}>
+                    <div className="display-name">{displayPost?.author?.name}</div>
+                    <div className="account-profile center" style={{backgroundImage: `url(${profilePic})`}} />
+                </Link>
                 {displayPost?.numberOfImages > 1 && <Row>
-                    <Col xs={6} className="left">
-                        <Button variant="contained" onClick={() => setDisplayImage((displayImage-1) < 0 ? displayPost?.numberOfImages-1 : (displayImage-1))}>Previous</Button>
+                    <Col xs={6} className="previous-image">
+                        <Button variant="contained" onClick={() => setDisplayImage(cur => (cur-1) < 0 ? displayPost?.numberOfImages-1 : (cur-1))}>Previous</Button>
                     </Col>
-                    <Col xs={6} className="right">
-                        <Button variant="contained" onClick={() => setDisplayImage((displayImage+1)%displayPost?.numberOfImages)}>Next</Button>
+                    <Col xs={6} className="next-image">
+                        <Button variant="contained" onClick={() => setDisplayImage(cur => (cur+1)%displayPost?.numberOfImages)}>Next</Button>
                     </Col>
                 </Row>}
             </Col>}
-            <Col xs={12} sm={12} md={6} className="info-container">
-                <Row>
-                    <Col xs={3} className="info-label">
-                        title:&nbsp;
-                    </Col>
-                    <Col xs={3} className="info-value">
-                        {displayPost?.title}
-                    </Col>
-                    <Col xs={3} className="info-label">
-                        price:&nbsp;
-                    </Col>
-                    <Col xs={3} className="info-value">
-                        {displayPost?.price}
-                    </Col>
-                </Row>
-                <Row>
-                    <Col xs={3} className="info-label">
-                        condition:&nbsp;
-                    </Col>
-                    <Col xs={3} className="info-value">
-                        {displayPost?.condition}
-                    </Col>
-                    <Col xs={3} className="info-label">
-                        brand:&nbsp;
-                    </Col>
-                    <Col xs={3} className="info-value">
-                        {displayPost?.brand}
-                    </Col>
-                </Row>
-                <Row>
-                    <Col xs={3} className="info-label">
-                        type:&nbsp;
-                    </Col>
-                    <Col xs={3} className="info-value">
-                        {displayPost?.type}
-                    </Col>
-                    <Col xs={3} className="info-label">
-                        username:&nbsp;
-                    </Col>
-                    <Col xs={3} className="info-value">
-                        <Link className="link" to={{pathname: '/profile', search: "id=" + displayPost?.author?.id}}>{displayPost?.author?.name}</Link>
-                    </Col>
-                    <Col xs={2} className="info-label">
-                        created:&nbsp;
-                    </Col>
-                    <Col xs={10} className="info-value">
-                        {createdDate}
-                    </Col>
-                </Row>
-                <Row>
-                    <Col xs={2} className="info-label">
-                        id:&nbsp;
-                    </Col>
-                    <Col xs={2} className="info-value">
-                        {displayPost?.id}
-                    </Col>
-                </Row>
+
+            <Col xs={12} className="center">
+                {createdDate}
             </Col>
+            
+            <Row>
+                <Col xs={12} sm={12} className="info-container">
+                    <Row>
+                        <Col xs={3}  className="info-label">
+                            <h2>{displayPost?.type}:&nbsp;</h2>
+                        </Col>
+                        <Col xs={3} className="info-value">
+                            <h2>
+                                $333{displayPost?.price}
+                            </h2>
+                        </Col>
+                        <Col xs={3} className="info-label">
+                            <h3>{displayPost?.condition}:</h3>
+                        </Col>
+                        <Col xs={3} className="info-value">
+                            <h3>{displayPost?.brand}</h3>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
         </Fragment>
     );
 }
