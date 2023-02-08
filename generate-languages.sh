8d21d7b93631d4bba2d0b96b9def836582ea7b42
Jacob Silverman <jacobsilverman@jacobs-mbp.lan>
2023-02-07 23:02:39 -0800

    adding arabic and fixing files

--- generate-languages.sh
+++ generate-languages.sh
@@ -4,13 +4,14 @@ export languages=$(node ./generate-languages.js);
 # var2=${PS1#*-}
 IFS="-"
 read -a strarr <<< "$languages"
-# echo ${strarr[0]} > ./src/Data/Assets/i18n/translations/en.json
-# echo ${strarr[1]} > ./src/Data/Assets/i18n/translations/es.json
-# echo ${strarr[2]} > ./src/Data/Assets/i18n/translations/iw.json
-echo ${strarr[0]}
-echo ${strarr[1]}
-echo ${strarr[2]}
-echo ${strarr[3]}
+echo ${strarr[0]} > ./src/Data/Assets/i18n/translations/en.json
+echo ${strarr[1]} > ./src/Data/Assets/i18n/translations/es.json
+echo ${strarr[2]} > ./src/Data/Assets/i18n/translations/iw.json
+echo ${strarr[3]} > ./src/Data/Assets/i18n/translations/ar.json
+# echo ${strarr[0]}
+# echo ${strarr[1]}
+# echo ${strarr[2]}
+# echo ${strarr[3]}
 # for val in "${strarr[@]}";
 # do
 #     printf "$val\n"
