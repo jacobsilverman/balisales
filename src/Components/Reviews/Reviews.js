3b62cec54d62d18b5e96afc58b7b397efa3e1840
Jacob Silverman <57313642+jacobsilverman@users.noreply.github.com>
2023-04-12 21:04:06 -0700

    starting to build the review and report process on a profile

--- src/Components/Reviews/Reviews.js
+++ src/Components/Reviews/Reviews.js
@@ -1,59 +1,92 @@
 import "./Reviews.scss";
 import {Col, Row} from "react-bootstrap";
 
-const Reviews = (userData) => {
+const Review = ({ name, id, message, date, rating }) => {
+    return (
+        <article className="review">
+            <a className="review-author" href={"/profile?id="+id}>{name}</a>
+            <div className="review-rating">Rating: <b className="review-rating-number">{rating}</b></div>
+            <div className="review-date">{date.toDateString()}</div>
+            <div className="review-message">{message}</div>
+        </article>
+    );
+};
 
+const ReviewsList = (reviews) => {
+    return (
+        <div className="reviews-list">
+        {reviews.map((review) => (
+            <Review
+                key={review.id}
+                name={review.author.name}
+                id={review.author.id}
+                message={review.message}
+                date={review.date}
+                rating={review.rating}
+                />
+        ))}
+        </div>
+    );
+};
+
+const Reviews = (userData) => {
     const reviews = [
         {
-            author: "jacob",
+            author: {
+                name: "Jacob",
+                id: "YrVu0gSQWIdyGPaWl25wgZNw9Jj1"
+            },
             message: "He was excellent with communication and helping to ensure the product was delivered",
             date: new Date(),
             rating: 5,
             id: 0
         },
         {
-            author: "jacob",
+            author: {
+                name: "Jacob",
+                id: "YrVu0gSQWIdyGPaWl25wgZNw9Jj1"
+            },
             message: "did what was needed but not the best",
             date: new Date(),
             rating: 4,
             id: 1
         },
         {
-            author: "jacob",
+            author: {
+                name: "Jacob",
+                id: "YrVu0gSQWIdyGPaWl25wgZNw9Jj1"
+            },
             message: "slow and unresponsive",
             date: new Date(),
             rating: 3,
-            id: 2
+            id: 12
+        },
+        {
+            author: {
+                name: "Jacob",
+                id: "YrVu0gSQWIdyGPaWl25wgZNw9Jj1"
+            },
+            message: "slow and unresponsive",
+            date: new Date(),
+            rating: 3,
+            id: 20
+        },
+        {
+            author: {
+                name: "Jacob",
+                id: "YrVu0gSQWIdyGPaWl25wgZNw9Jj1"
+            },
+            message: "slow and unresponsive",
+            date: new Date(),
+            rating: 3,
+            id: 21
         },
     ];
 
-    const allR = reviews.map((ele) => {
-        return (
-            <div key={ele?.id}>
-                <div className="review-item">
-                    <div>
-                        {ele?.author}
-                    </div>
-                    <div className="center">
-                        {ele?.message}
-                    </div>
-                </div>
-                <hr style={{margin:"0"}} />
-            </div>
-        );
-    });
-
-
     return (<Row className="reviews-container">
-        {/* <Col xs={6}  className="right-border-md">
-            <div className="center">
-                <h4>Reports</h4>
-            </div>
-        </Col> */}
-        <Col xs={(userData?.reports) ? 6 : 12}>
+        <Col xs={12}>
             <h3>Reviews</h3>
-            <hr style={{margin:"0"}} />
-            {allR}
+            {ReviewsList(reviews)}
         </Col>
     </Row>);
 }
