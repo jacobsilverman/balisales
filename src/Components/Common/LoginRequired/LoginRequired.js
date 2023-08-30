87341b4ff3621c77c51ef9d4c1d0695a0f6036a7
Jacob Silverman <57313642+jacobsilverman@users.noreply.github.com>
2023-08-29 18:24:26 -0700

    protecting the create post and settings routes but it just redirects to the home page when a login page is still able to be made

---
+++ src/Components/Common/LoginRequired/LoginRequired.js
@@ -0,0 +1,30 @@
+import './LoginRequired.scss';
+import { useEffect } from 'react';
+import { Container , Col, Row} from "react-bootstrap";
+
+import { useTranslation } from "react-i18next";
+
+export const LoginRequired = () => {
+    const { t } = useTranslation();
+
+    return (
+        <>
+            <Container className="login-header-container">
+                <Row>
+                    <Col className="login-header">
+                        <h2>{t("Information Missing")}</h2>
+                    </Col>
+                </Row>
+            </Container>
+            <Container className="require-login-info">
+                <Row>
+                    <Col xs={12}>
+                        <h3>
+                            Please login before trying to continue
+                        </h3>
+                    </Col>
+                </Row>
+            </Container>
+        </>
+    );
+}
